c0a325756b265efea9a42482ac81884153cb59ee
shubham agarwal <82525103+Shubham323201@users.noreply.github.com>
2022-08-03 15:09:11 +0530

    add enviroment vard

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const userSchema=new mongoose.Schema({
   password:String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret:process.env.SECRET,encryptedFields: ["password"] });
 
 
 
